928e98b2982248a0ffc558c25eaac66da4982484
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2020-05-29 14:11:31 +0200

    cette fois on est bon !

---
+++ API/TMDBApi.js
@@ -0,0 +1,14 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "eddffa33a17a5088c01f3001bfbac2df";
+
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+    return fetch(url)
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+return 'https://image.tmdb.org/t/p/w300' + name
+}
